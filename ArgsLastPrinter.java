461edffd8711442ef4d43c2f938a817b17863e1c
Taro <taro@example.com>
2023-10-11 17:10:17 +0900

    課題提出

---
+++ ArgsLastPrinter.java
@@ -0,0 +1,9 @@
+public class ArgsLastPrinter{
+    public static void main(String[] args){
+        if(args.length == 0){
+        System.out.println("なし");
+        }else if (args.length >= 0) {
+        System.out.println("表示あり");
+        } 
+    }
+}
